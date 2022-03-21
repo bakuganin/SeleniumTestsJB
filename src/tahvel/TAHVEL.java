e1e1dbe45781f916fcbd83ef9130bd289a8a1a36
Jegor <kafder97@gmail.com>
2022-03-21 11:11:17 +0200

    SMART ID login

--- src/tahvel/TAHVEL.java
+++ src/tahvel/TAHVEL.java
@@ -17,30 +17,38 @@ import org.openqa.selenium.support.ui.Select;
  */
 
 public class TAHVEL {
-
+    
     private static WebDriver driver = null;
     
     public static void main(String[] args) throws InterruptedException {
+//        
+//        char passwordArray[] = System.console().readPassword("Enter password: ");
         
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Desktop\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://tahvel.edu.ee/");
         driver.manage().window().maximize();
         
+        
+        // Логин 
         driver.findElement(By.className("user-section")).click();
         Thread.sleep(1000);
-        driver.findElement(By.cssSelector("a[href='https://tahvel.edu.ee/hois_back/haridLogin']")).click();
-        
-        driver.findElement(By.className("input-email-field")).sendKeys("kafder97@gmail.com");;
+        driver.findElement(By.xpath("/html/body/div[3]/md-dialog/md-dialog-content/div/div/div/div[1]/button[2]")).click();
         Thread.sleep(1000);
-        driver.findElement(By.className("input-field")).sendKeys("5051Pines");
+        driver.findElement(By.xpath("/html/body/div[3]/md-dialog/md-dialog-content/div/md-content[4]/a")).click();
         Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div/div/div[3]/div[2]/nav/ul/li[3]/a/span")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div/div/div[3]/div[2]/main/div[3]/div/div[1]/div[2]/form/table/tbody/tr[1]/td[2]/div[1]/input")).click();
+        Thread.sleep(1000);
+        WebElement password = driver.findElement(By.xpath("/html/body/div/div/div[3]/div[2]/main/div[3]/div/div[1]/div[2]/form/table/tbody/tr[1]/td[2]/div[1]/input"));
+        password.sendKeys("50201233737");
         
-        driver.findElement(By.className("btn-primary")).click();
-        //Select se = new Select(driver.findElement(By.xpath("//*[@class='add-btns-group']")));
-        //se.selectByValue("Logi sisse");
-//      driver.findElement(By.className("btn btn-primary ml-lg-3")).click();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("/html/body/div/div/div[3]/div[2]/main/div[3]/div/div[1]/div[2]/form/table/tbody/tr[2]/td[2]/button")).click();
+        
+        // Открытие дневника
+        Thread.sleep(25000);
         driver.navigate().to("https://tahvel.edu.ee/#/students/journals");   
         Thread.sleep(5000);
         driver.quit();
